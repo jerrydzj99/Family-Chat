d0131dfe07c203843e2c2af4e3887c74ae2ab272
Jerry Ding <jerryding@Jerrys-MacBook-Pro.local>
2018-05-05 11:38:10 -0700

    design welcome screen, set up welcome, login, and register view controllers

--- Family Chat/WelcomeViewController.swift
+++ Family Chat/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Family Chat
 //
 //  Created by Jerry Ding on 2018-05-05.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
